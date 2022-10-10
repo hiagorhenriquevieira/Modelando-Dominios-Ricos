053d7f03e25aa912809abe25d889b940e8afb04a
Hiagor Vieira <hiagor.vieira@inovafarma.com.br>
2022-10-10 07:25:01 -0300

    criacao de uma querie

---
+++ Modelando/Modelando.Domain/Queries/EstudanteQueries.cs
@@ -0,0 +1,14 @@
+﻿using Modelando.Domain.Entities.EstudanteEntities;
+using System;
+using System.Linq.Expressions;
+
+namespace Modelando.Domain.Queries
+{
+    public static class EstudanteQueries
+    {
+        public static Expression<Func<Estudante, bool>> AtribuirInformacoesEstudante(string documento)
+        {
+            return x => x.Documento.Numero == documento; 
+        }
+    }
+}
